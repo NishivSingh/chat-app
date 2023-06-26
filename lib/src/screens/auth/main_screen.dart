25a30a001c58fd39dbe8f255b701b90734bef644
Nishiv Singh <singh.136@iitj.ac.in>
2023-06-26 10:12:59 +0530

    Added the welcome and login page

---
+++ lib/src/screens/auth/main_screen.dart
@@ -0,0 +1,34 @@
+import 'package:chat_app/src/screens/auth/signin_screen.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.red,
+              height: screenHeight*0.6),
+            Row(
+              children: [
+                Expanded(child: OutlinedButton(onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const SignIn()));
+                }, child: const Text("LOGIN"))),
+                const SizedBox(width: 50,),
+                Expanded(child: ElevatedButton(onPressed: (){}, child: const Text("SIGNUP"))),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
