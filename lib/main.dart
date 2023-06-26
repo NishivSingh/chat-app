3d21446b684bd2c205ae849749ac5f990f29e8a8
Nishiv Singh <singh.136@iitj.ac.in>
2023-06-26 10:13:41 +0530

    some changes to new screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/src/screens/auth/main_screen.dart';
 import 'package:chat_app/src/utils/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -21,33 +22,9 @@ class MyApp extends StatelessWidget {
       title: 'Chat App',
       debugShowCheckedModeBanner: false,
       theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-      themeMode: ThemeMode.system,
-      home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatefulWidget{
-  const HomeScreen({super.key});
-  @override
-  HomeScreenState createState() => HomeScreenState();
-}
-
-class HomeScreenState extends State<HomeScreen>{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Center(child: Text("Flutter app"))),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView(children: [
-          Container(color: Colors.red,height: 50,),
-          Container(color: Colors.blue,height: 50,),
-          Container(color: Colors.green,height: 50,),
-          Container(color: Colors.yellow,height: 50,),
-        ]),
-      ),
+      // darkTheme: TAppTheme.darkTheme,
+      // themeMode: ThemeMode.system,
+      home: const MainScreen(),
     );
   }
 }
--- lib/src/utils/theme.dart
+++ lib/src/utils/theme.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class TAppTheme {
-  static ThemeData lightTheme = ThemeData(brightness: Brightness.light);
-  static ThemeData darkTheme = ThemeData(brightness: Brightness.dark);
+
+  TAppTheme._();
+  
+  static ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    textTheme: TextTheme(displayMedium: GoogleFonts.montserrat(
+      color: Colors.blueGrey,
+    )));
+
+
+
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    textTheme: TextTheme(displayMedium: GoogleFonts.montserrat(
+      color: Colors.blueGrey,
+    )));
 }
