91bf613bdcfb6a9e06a2bd41c8c7eee243aa23c5
Nishiv Singh <singh.136@iitj.ac.in>
2023-06-29 23:05:09 +0530

    Improved the functionality of some widgets

--- lib/src/screens/auth/sign_in/signin_screen.dart
+++ lib/src/screens/auth/sign_in/signin_screen.dart
@@ -1,5 +1,7 @@
 import 'package:chat_app/src/reusable_widgets/reusable_form_footer.dart';
 import 'package:chat_app/src/screens/auth/sign_in/signin_screen_widgets.dart';
+import 'package:chat_app/src/screens/auth/sign_up/signup_screen.dart';
+import 'package:chat_app/src/utils/constants/text.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatelessWidget {
@@ -18,7 +20,7 @@ class SignIn extends StatelessWidget {
                 color: Colors.green,
               ),
               const LoginForm(),
-              const FormFooterWidget()
+              const FormFooterWidget(text1: dontHaveAnAccount,text2:signUp,page: SignUp(),)
             ],
           ),
         ),
--- lib/src/screens/auth/sign_in/signin_screen_widgets.dart
+++ lib/src/screens/auth/sign_in/signin_screen_widgets.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/src/utils/constants/size.dart';
+import 'package:chat_app/src/utils/constants/text.dart';
 import 'package:flutter/material.dart';
 
 class LoginForm extends StatelessWidget {
@@ -13,8 +15,8 @@ class LoginForm extends StatelessWidget {
         const TextField(
           decoration: InputDecoration(
               prefixIcon: Icon(Icons.person_outline_outlined),
-              labelText: "Email",
-              hintText: "Email",
+              labelText: email,
+              hintText: email,
               border: OutlineInputBorder()),
         ),
         const SizedBox(
@@ -23,8 +25,8 @@ class LoginForm extends StatelessWidget {
         const TextField(
           decoration: InputDecoration(
               prefixIcon: Icon(Icons.fingerprint),
-              labelText: "Password",
-              hintText: "Password",
+              labelText: password,
+              hintText: password,
               border: OutlineInputBorder(),
               suffixIcon: IconButton(
                   onPressed: null, icon: Icon(Icons.remove_red_eye_sharp))),
@@ -35,14 +37,24 @@ class LoginForm extends StatelessWidget {
         Align(
             alignment: Alignment.centerRight,
             child: TextButton(
-                onPressed: () {}, child: const Text("Forget password?"))),
+                onPressed: () {
+                  showModalBottomSheet(context: context, builder: (context)=>Container(
+                    padding: const EdgeInsets.all(defaultSize),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(forgetPasswordTitle)
+                      ],
+                    ),
+                  ));
+                }, child: const Text(forgetPassword))),
         const SizedBox(
           height: 10,
         ),
         SizedBox(
             width: double.infinity,
             child:
-                ElevatedButton(onPressed: () {}, child: const Text("LOGIN"))),
+                ElevatedButton(onPressed: () {}, child: Text(login.toUpperCase()))),
       ],
     ),
   ));
